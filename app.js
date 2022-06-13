4fb0d881df2999eddad056b77491328b783fe577
Erika Pauwels <erika.pauwels@gmail.com>
2022-06-13 15:15:26 +0200

    Add additional filter on incoming delta's
    Make sure to only handle mailchimp related delta's

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 import { app, errorHandler } from 'mu';
 import { getNotStartedPublicationTasks, TASK_ONGOING_STATUS } from './lib/publication-task';
+import bodyParser from 'body-parser';
+import { MAILCHIMP_PUBLICATION_CHANNEL } from './config';
+
+/* Accept application/json format from delta-notifier */
+app.use(bodyParser.json({
+  type: function(req) { return /^application\/json/.test(req.get('content-type')); }
+}));
+
 
 const requiredEnvironmentVariables = [
   'MAILCHIMP_API',
@@ -20,21 +28,33 @@ requiredEnvironmentVariables.forEach((key) => {
 if (error) process.exit(1);
 
 app.post('/delta', async function (req, res, next) {
-  console.log("Processing deltas for Mailchimp...");
+  const delta = req.body;
+  console.log(`Receiving delta ${JSON.stringify(delta)}`);
 
-  const publicationTasks = await getNotStartedPublicationTasks();
+  const objects = delta
+        .map((changeset) => changeset.inserts)
+        .flat()
+        .map((triple) => triple.object.value);
+  if (objects.find(v => v == MAILCHIMP_PUBLICATION_CHANNEL)) {
+    console.log("Processing deltas for Mailchimp...");
 
-  if (publicationTasks) {
-    console.log(`Found ${publicationTasks.length} publication tasks to be processed.`);
-    for (const publicationTask of publicationTasks) {
-      await publicationTask.persistStatus(TASK_ONGOING_STATUS);
-    };
-    res.sendStatus(202);
-    for (const publicationTask of publicationTasks) {
-      await publicationTask.process();
-    };
+    const publicationTasks = await getNotStartedPublicationTasks();
+
+    if (publicationTasks) {
+      console.log(`Found ${publicationTasks.length} publication tasks to be processed.`);
+      for (const publicationTask of publicationTasks) {
+        await publicationTask.persistStatus(TASK_ONGOING_STATUS);
+      };
+      res.sendStatus(202);
+      for (const publicationTask of publicationTasks) {
+        await publicationTask.process();
+      };
+    } else {
+      console.log(`No publication tasks found to be processed.`);
+      return res.status(200).end();
+    }
   } else {
-    console.log(`No publication tasks found to be processed.`);
+    console.log(`Delta message doesn't contain an insert for Mailchimp publication channel`);
     return res.status(200).end();
   }
 });
